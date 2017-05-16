e4582194b81ceda74b674eb218be7c1b976729d6
Matt Matuszak <mattmatuszak@northwesternmutual.com>
2017-05-16 12:02:50 -0500

    added dynamic map

--- scripts/map.js
+++ scripts/map.js
@@ -1,12 +1,44 @@
 
 function buildMap() {
 
-  // let map = [
-  //   [1,1,1,1,1,1,1,1,1,1]
-  //   ,
-  // ];
+// 0-empty
+// 1-brick
+// 2-coin
+
+  let map = [
+     [1,1,1,1,1,1,1,1,1,1]
+    ,[1,2,2,2,2,2,2,2,2,1]
+    ,[1,2,2,2,2,2,2,2,2,1]
+    ,[1,2,2,2,2,2,2,2,2,1]
+    ,[1,2,2,2,3,2,2,2,2,1]
+    ,[1,2,2,2,2,2,2,2,2,1]
+    ,[1,2,2,2,2,2,2,2,2,1]
+    ,[1,2,2,2,2,2,2,2,2,1]
+    ,[1,1,1,1,1,1,1,1,1,1]
+  ];
+
+  let html = '';
+  for(let row = 0; row < map.length; row++) {
+    html += `<div class='row'>`
+    for (var column = 0; column < map[row].length; column++) {
+      html += `<div class='${getCssClass(map[row][column])}' />`;
+    }
+    html += `</div>`
+
+  }
+  console.log(html);
+
+  $('.container').html(html);
 
   return map;
 
 
+}
+
+function getCssClass(code) {
+  if (code === 0) return 'empty';
+  else if (code === 1) return 'brick';
+  else if (code === 2) return 'coin';
+  else if (code === 3) return 'pacman';
+  return 'fixme';
 }
